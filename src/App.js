97c41fd363a5a4e97739480c9c3604d0974fb302
Suhas1104 <suhas1104@gmail.com>
2023-07-21 20:39:05 +0530

    completed for sure modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from "react";
+
+function Square({ value, handleClick }) {
+  const [val, setVal] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
   );
 }
 
-export default App;
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    function calcwin(square){
+      const lines=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+      ]
+      for(let i=0;i<lines.length;i++)
+      {
+        const [a,b,c]=lines[i]
+        if(square[a]&&square[a]==square[b]&&square[b]==square[c])
+        return square[a]
+       
+      }
+      return null
+    }
+    const winner = calcwin(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  function handleClick(i) {
+    const nextSquares = squares.map(curr=>curr);
+    if (squares[i]||calcwin(squares)) {
+      return;
+    }
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext)
+    setSquares(nextSquares);
+  }
+  return (
+    <>
+    <h1>{status}</h1>
+      <div className="board-row">
+        <Square value={squares[0]} handleClick={()=>handleClick(0)} />
+        <Square value={squares[1]}  handleClick={()=>handleClick(1)} />
+        <Square value={squares[2]}  handleClick={()=>handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]}  handleClick={()=>handleClick(3)} />
+        <Square value={squares[4]}  handleClick={()=>handleClick(4)} />
+        <Square value={squares[5]}  handleClick={()=>handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]}  handleClick={()=>handleClick(6)}/>
+        <Square value={squares[7]}  handleClick={()=>handleClick(7)} />
+        <Square value={squares[8]}  handleClick={()=>handleClick(8)} />
+      </div>
+      <div className="board-row">
+        <button onClick={()=>setSquares([].fill(null))}>reset</button>
+      </div>
+    </>
+  );
+}
+
